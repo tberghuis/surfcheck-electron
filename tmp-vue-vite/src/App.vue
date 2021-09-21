495573d1c537b804e74f9c4a296ea1449263eea1
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-09-21 13:54:09 +1000

    .

--- tmp-vue-vite/src/App.vue
+++ tmp-vue-vite/src/App.vue
@@ -3,25 +3,33 @@ import Surfcam from "./components/Surfcam.vue";
 import { ref, onMounted, watch } from "vue";
 
 const camUrls = [
+  "https://cams.cdn-surfline.com/cdn-au/au-byronbay/playlist.m3u8",
+  "https://cams.cdn-surfline.com/cdn-au/au-thepassoverview/playlist.m3u8",
   "https://cams.cdn-surfline.com/cdn-au/au-lennoxhead/playlist.m3u8",
+  "https://cams.cdn-surfline.com/cdn-au/au-ballinashellybeach/playlist.m3u8",
   "https://cams.cdn-surfline.com/cdn-au/au-ballinalighthouse/chunklist.m3u8",
+  "https://cams.cdn-surfline.com/cdn-au/au-evanshead/playlist.m3u8",
 ];
 
 const camIndex = ref(0);
 
-const toggleCam = () => {
-  camIndex.value++;
-  console.log("camIndex", camIndex.value);
-  camIndex.value = camIndex.value % 2;
+const toggleCam = (index) => {
+  // camIndex.value++;
+  // console.log("camIndex", camIndex.value);
+  // camIndex.value = camIndex.value % 2;
 };
 </script>
 
 <template>
-  <div>
-    <div>hello</div>
-    <button @click="toggleCam">toggle cam</button>
+  <div class="cam-list">
+    <button @click="camIndex = 0">the wreck</button>
+    <button @click="camIndex = 1">the pass</button>
+    <button @click="camIndex = 2">lennox</button>
+    <button @click="camIndex = 3">shelly</button>
+    <button @click="camIndex = 4">northwall</button>
+    <button @click="camIndex = 5">evans</button>
   </div>
-  <div>
+  <div class="cam-container">
     <Surfcam :camUrl="camUrls[camIndex]" />
   </div>
 </template>
@@ -32,4 +40,20 @@ const toggleCam = () => {
   /* max-width: 100vw; */
 }
 
+.cam-list {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  max-width: 200px;
+  min-width: 200px;
+  /* flex-basis: 2; */
+}
+
+.cam-list button {
+  width: 100%;
+}
+
+.cam-container {
+  /* flex-basis: 4; */
+}
 </style>
---
+++ tmp-vue-vite/src/components/BeachList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+// I should rewrite in react as my mind can wrap my head around it easier
+// also use typescript
+
+defineProps({
+  onClickCam: Function
+})
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
